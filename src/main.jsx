93920a1b07d370fba6a89b06fc2bb354692f9bd3
nafilahhans <hanshalifan12@gmail.com>
2026-05-18 23:11:15 +0700

    atomic design page sign in dan sign up

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 //import Exercise from './Exercise1.jsx'
-import Exercise from './Exercise.jsx'
+//import Exercise from './Exercise.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Exercise/>
+    <App/>
   </React.StrictMode>,
 )
