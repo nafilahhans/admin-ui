d78d97432a740d748f6e581d884e9471ec9f4a88
nafilahhans <hanshalifan12@gmail.com>
2026-06-08 21:25:23 +0700

    with context

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { ThemeContextProvider } from "./context/themeContext.jsx";
+//import Parent from "./latihan/Parent.jsx";
+//import { CounterContextProvider } from "./context/counterContext.jsx";
 //import Exercise from './Exercise1.jsx'
 //import Exercise from './Exercise.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App/>
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
   </React.StrictMode>,
-)
+);
