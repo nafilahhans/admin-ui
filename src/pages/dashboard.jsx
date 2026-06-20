4e8d955a2ff29a92edf2e069263dbf6773d5eb3c
nafilahhans <hanshalifan12@gmail.com>
2026-06-21 00:51:45 +0700

    User Interaction

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -16,18 +16,32 @@ import {
  } from "../data";
 import { goalService } from "../services/dataService";
 import { AuthContext } from "../context/authContext";
-
+import AppSnackbar from "../components/Elements/AppSnackbar";
 
 function dashboard() {
   const [goals, setGoals] = useState({});
   const { logout } = useContext(AuthContext);
+
+  	const [snackbar, setSnackbar] = useState({
+    open: false,
+    message: "",
+    severity: "success",
+  }); 
+  
+  const handleCloseSnackbar = () => {
+    setSnackbar((prev) => ({ ...prev, open: false }));
+  };
 
   const fetchGoals = async () => {
     try {
       const data = await goalService();
       setGoals(data);
     } catch (err) {
-      console.error("Gagal mengambil data goals:", err);
+      setSnackbar({ 
+        open: true, 
+        message: "Gagal mengambil data goals", 
+        severity: "error" 
+      });
       if (err.status === 401) {
         logout();
       }
@@ -63,6 +77,13 @@ function dashboard() {
             <CardExpenseBreakdown data={expensesBreakdowns} />
           </div>
         </div>
+
+        <AppSnackbar
+          open={snackbar.open}
+          message={snackbar.message}
+          severity={snackbar.severity}
+          onClose={handleCloseSnackbar}
+        />
     </MainLayout>
     
     </>
